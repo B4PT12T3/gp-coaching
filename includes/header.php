96d55315708100a139fe74cd6a059bc76f81f7ee
B4PT12T3 <125573072+B4PT12T3@users.noreply.github.com>
2026-09-16 09:21:03 +0200

    test tag manager

--- includes/header.php
+++ includes/header.php
@@ -80,7 +80,7 @@ function nav_active(string $file): string
         j.src =
           'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
         f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', 'GTM-5GPFXK2R');
+      })(window, document, 'script', 'dataLayer', 'GTM-T9MLXGHQ');
     }
   </script>
   <!-- End Google Tag Manager -->
